efbdb8daf8232171c034ad6ef306ac6613e9739d
nelsonfoster <140848049+nelsonfoster-216@users.noreply.github.com>
2025-05-16 14:31:07 -0400

    deployment fixes again

--- cleveland-tech-dashboard/server.js
+++ cleveland-tech-dashboard/server.js
@@ -1,28 +1,19 @@
 const path = require('path');
-const { createServer } = require('http');
-const { parse } = require('url');
 const next = require('next');
+const { createServer } = require('http');
 
-const dev = process.env.NODE_ENV !== 'production';
-const hostname = process.env.HOSTNAME || 'localhost';
-const port = process.env.PORT || 3000;
+const app = next({ 
+  dev: false, 
+  dir: path.join(__dirname) 
+});
 
-// Create the Next.js app
-const app = next({ dev, dir: __dirname });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      const parsedUrl = parse(req.url, true);
-      await handle(req, res, parsedUrl);
-    } catch (err) {
-      console.error('Error occurred handling', req.url, err);
-      res.statusCode = 500;
-      res.end('Internal Server Error');
-    }
-  }).listen(port, (err) => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(process.env.PORT || 3000, (err) => {
     if (err) throw err;
-    console.log(`> Ready on http://${hostname}:${port}`);
+    console.log(`> Ready on http://localhost:${process.env.PORT || 3000}`);
   });
 }); 
